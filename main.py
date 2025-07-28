afc57a6b84b624b5c9e4d73fda4bb0215df09f57
Vzhdanov95 <vgdanov@gmail.com>
2025-07-28 19:36:21 +0300

    added collections to main.py file

---
+++ main.py
@@ -0,0 +1,12 @@
+# Mutable data types
+names = ["Alyona", "Adam", "Alice"]
+cities_countries = {"New York": "United States", "Kyiv": "Ukraine", "Deli": "India"}
+unique_ids = {124343, 38983493, 47583834, 37783394, 37783394}
+print(len(unique_ids))
+print(unique_ids)
+
+# Immutable data types 
+
+
+company_names_nt_change = ("Desert inc.", "Black eagle.ent", "Blizzard")
+print(company_names_nt_change)
