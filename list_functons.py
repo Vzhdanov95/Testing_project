11829a934b596b18bc5ae6fbdc2986cf78f73efb
Vzhdanov95 <vgdanov@gmail.com>
2025-07-31 20:10:58 +0300

    updated_collection_files

--- list_functons.py
+++ list_functons.py
@@ -1,5 +1,5 @@
 object_list = [1, 3, 4, 5, 56, 9, 23, 9]
-# print(object_list.index(9))
+# print(object_list.index(4))
 
 # object_list.append(44)
 # object_list.append(99)
@@ -7,21 +7,27 @@ object_list = [1, 3, 4, 5, 56, 9, 23, 9]
 # object_list.remove(9)
 # print(object_list)
 
+    # от большого к маленькому если reverse=True
 # object_list.sort(reverse=True)
-# # print(object_list)
+# print(object_list)
 
 # object_lsit_1 = object_list.copy()
 # print(object_lsit_1)
 # print(object_list)
 
-
+    # Подсчет количества значений
 # print(object_list.count(9)) 
 
 # subject_list = ["Breathe", 5555]  # вместо использования "+"" в обьеденении листов
 # object_list.extend(subject_list)
 # print(object_list)
 
-object_list.insert(3, "this is five:")
+# object_list.insert(3, "this is five:")
 
-object_list.pop(3) # удалили то что заинсертили методом insert() выше
-print(object_list)
+# object_list.pop(3) # удалили то что заинсертили методом insert() выше
+# print(object_list)
+
+chekck_some = [33, 44, 55, 88, 6, 9]
+chekck_some.pop(2)
+chekck_some.remove(88)
+print(chekck_some)
