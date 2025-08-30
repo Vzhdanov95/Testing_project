bd965c35b64ed3379651cc39d4b0776c451c1e6d
Vzhdanov95 <vgdanov@gmail.com>
2025-08-31 00:40:47 +0300

    check

---
+++ strings/str_functions.py
@@ -0,0 +1,13 @@
+str_int = '445'
+str_float = "22.09"
+str_byte = '00993'
+some_name = 'Zhdanov Vadym'
+
+# print(some_name.find('a', 4, 10)) # находит по первому вхождению, find('', start, end)
+# print(str_int.isdigit())
+# print(str_float.isdigit()) # False потому что воспринимает точку как не цифру
+# print(str_byte.isdigit())
+# print(some_name.index("a"))
+# print(some_name.rindex("a"))
+# print(some_name.casefold())
+# print(some_name.lower())
---
+++ strings/str_slicing.py
@@ -0,0 +1,7 @@
+my_name = 'Vadym' # Стринга под капотом условный Tuple тоесть immutable
+# print(my_name[0])
+
+print(my_name[2:5]) # слайсинг, последнее не включительно.
+print(my_name[0:5:2]) # 2 здесь это шаг
+print(my_name[-1])  # Обратная индексация
+print(my_name[::-1]) # Переворот стринги
